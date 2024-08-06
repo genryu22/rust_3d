4267a9f6ad76bf92e827397a01ef94eb8b60b63a
Genryu Inami <genryu7@gmail.com>
2024-08-06 23:45:40 +0900

    #64 ウインドウを表示

--- examples/raw_input.rs
+++ examples/raw_input.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 #[cfg(target_os = "macos")]
 fn main() {
     use core_graphics::event::{CGEvent, CGEventField, CGEventType};
@@ -18,6 +20,93 @@ fn main() {
 }
 
 #[cfg(target_os = "windows")]
-fn main() {
-    use windows::Win32::UI::Input;
+use windows::{
+    core::*,
+    Win32::{
+        Foundation::*, Graphics::Gdi::*, System::LibraryLoader::*, UI::Input::*,
+        UI::WindowsAndMessaging::*,
+    },
+};
+
+#[cfg(target_os = "windows")]
+fn main() -> Result<()> {
+    unsafe {
+        let instance = GetModuleHandleW(None)?;
+        let window_class = w!("window");
+
+        let class = WNDCLASSEXW {
+            cbSize: mem::size_of::<WNDCLASSEXW>() as u32,
+            style: CS_HREDRAW | CS_VREDRAW,
+            lpfnWndProc: Some(wndproc),
+
+            hInstance: instance.into(),
+            hCursor: LoadCursorW(None, IDC_ARROW)?,
+
+            lpszClassName: window_class,
+
+            ..Default::default()
+        };
+        let atom = RegisterClassExW(&class);
+        debug_assert!(atom != 0);
+
+        let hwndMain = CreateWindowExW(
+            WINDOW_EX_STYLE::default(),
+            w!("window"),
+            w!("Test"),
+            WS_OVERLAPPEDWINDOW | WS_HSCROLL | WS_VSCROLL,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            Option::None,
+            Option::None,
+            instance,
+            Option::None,
+        )?;
+
+        let mouse = RAWINPUTDEVICE {
+            usUsagePage: 0x01,
+            usUsage: 0x02,
+            dwFlags: RAWINPUTDEVICE_FLAGS::default(),
+            hwndTarget: hwndMain,
+        };
+
+        let res = RegisterRawInputDevices(&[mouse], mem::size_of::<RAWINPUTDEVICE>() as u32);
+        if let Ok(_) = res {
+            println!("ok");
+        }
+
+        let _ = ShowWindow(hwndMain, SW_SHOWDEFAULT);
+
+        let mut msg = MSG::default();
+        while GetMessageW(&mut msg, None, 0, 0).into() {
+            let _ = TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(target_os = "windows")]
+extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    unsafe {
+        match message {
+            WM_PAINT => {
+                println!("WM_PAINT");
+                _ = ValidateRect(window, None);
+                LRESULT(0)
+            }
+            WM_INPUT => {
+                println!("WM_INPUT");
+                LRESULT(0)
+            }
+            WM_DESTROY => {
+                println!("WM_DESTROY");
+                PostQuitMessage(0);
+                LRESULT(0)
+            }
+            _ => DefWindowProcW(window, message, wparam, lparam),
+        }
+    }
 }
